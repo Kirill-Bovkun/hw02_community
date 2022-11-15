7b822aae1c9dea848bddba32fa24f21b67e55dae
Kirill Bovkun <mr.bovkun.k@gmail.com>
2022-11-15 23:54:27 +0700

    Change signup.html, add app Core

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -11,6 +11,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'posts.apps.PostsConfig',
     'users.apps.UsersConfig',
+    'core.apps.CoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
