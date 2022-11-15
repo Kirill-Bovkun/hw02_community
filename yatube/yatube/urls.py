17f38ff0b7404990e43a7ae21b2929a2bc4b154b
Kirill Bovkun <mr.bovkun.k@gmail.com>
2022-11-15 19:31:55 +0700

    Add /auth in urls.py

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
+    path('auth/', include('django.contrib.auth.urls')),
 ]
