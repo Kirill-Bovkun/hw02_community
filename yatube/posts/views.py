3e8ca93619d8acbe1abc350cec011e3cd658d078
Kirill Bovkun <mr.bovkun.k@gmail.com>
2022-11-18 17:37:13 +0700

    Make something, idk

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,18 +1,37 @@
 from .models import Post, Group
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
+from functools import wraps
+
 
 LIMIT: int = 10
 
 
+def authorized_only(func):
+    @wraps(func)
+    def check_user(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return func(request, *args, **kwargs)
+        return redirect('/auth/login/')
+    return check_user
+
+
+@authorized_only
 def index(request):
     template = 'posts/index.html'
-    posts = Post.objects.all()[:LIMIT]
+    posts = Post.objects.all()
+    paginator = Paginator(posts, LIMIT)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
         'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, template, context)
 
 
+@authorized_only
 def group_posts(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -45,6 +45,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'core.context_processors.year.year',
             ],
         },
     },
@@ -87,3 +88,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+LOGIN_URL = 'users:login'
+
+LOGIN_REDIRECT_URL = 'posts:index'
+
+#LOGOUT_REDIRECT_URL = 'posts:index'
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
